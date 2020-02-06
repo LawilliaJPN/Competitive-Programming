1a8f8d3aae4ff35aeb0ed0c149844bfce05565fe
Lawillia <30737406+LawilliaJPN@users.noreply.github.com>
2020-02-06 09:19:47 +0900

    [FIX] ABC 153 - A - C#

--- AtCoder/ABC153/ABC153A.cs
+++ AtCoder/ABC153/ABC153A.cs
@@ -1,6 +1,6 @@
 using System;
 
-public class ABC137_A {
+public class ABC153_A {
     private static void Main(string[] args) {
         var inputs = Console.ReadLine().Trim().Split();
         var h = double.Parse(inputs[0]);
