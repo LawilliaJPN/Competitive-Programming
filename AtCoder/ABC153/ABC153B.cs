9d5357d41f1b6d406d9c4723058662c5909177b2
Lawillia <30737406+LawilliaJPN@users.noreply.github.com>
2020-02-07 07:54:51 +0900

    [UPDATE] ABC 153 - B - C#

--- AtCoder/ABC153/ABC153B.cs
+++ AtCoder/ABC153/ABC153B.cs
@@ -3,15 +3,11 @@ using System.Linq;
 
 public class ABC153_B {
     private static void Main(string[] args) {
-        var inputs1 = Read1LineAsIntArray();
-        var h = inputs1[0];
-        var n = inputs1[1];
+        var h = Read1LineAsIntArray()[0];
         var a = Read1LineAsIntArray();
 
-        if (a.Sum() >= h)
-            Console.WriteLine("Yes");
-        else
-            Console.WriteLine("No");
+        var answer = (a.Sum() >= h) ? "Yes" : "No";
+        Console.WriteLine(answer);
     }
   
     private static int[] Read1LineAsIntArray() => Console.ReadLine().Trim().Split().Select(x => int.Parse(x)).ToArray();
