4efa41ed3234850738e00bbdc84da4b01b1a6625
Lawillia <30737406+LawilliaJPN@users.noreply.github.com>
2020-04-19 15:39:42 +0900

    [NEW] ABC 162 - C - C#

---
+++ AtCoder/ABC162/ABC162C.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ABC162C {
+	private static void Main(string[] args) {
+		var k = ReadInt();
+		var answer = 0;
+
+		for (int a = 1; a <= k; a++) {
+			for (int b = 1; b <= k; b++) {
+				for (int c = 1; c <= k; c++) {
+					answer += CalcGCD(a, b, c);
+				}
+			}
+		}
+
+		Console.WriteLine(answer);
+	}
+
+	private static int ReadInt() => int.Parse(Console.ReadLine().Trim());
+	private static int GCD(int a, int b) => b == 0 ? a : GCD(b, a % b);
+	private static int CalcGCD(int a, int b) => a > b ? GCD(a, b) : GCD(b, a);
+	private static int CalcGCD(int a, int b, int c) => CalcGCD(CalcGCD(a, b), c);
+}
--- AtCoder/ABC162/README.md
+++ AtCoder/ABC162/README.md
@@ -4,5 +4,6 @@
 ## 参加範囲
 - A - Lucky 7
 - B - FizzBuzz Sum
+- C - Sum of gcd of Tuples (Easy)
 ## 参加言語
-- C# : A, B
+- C# : A, B, C
