c363641a75017331e6b3bdefac92b5e94800c4be
Lawillia <30737406+LawilliaJPN@users.noreply.github.com>
2020-07-07 16:28:44 +0900

    [UPDATE] ABC 165 - A - C++

--- AtCoder/ABC165/ABC165A.cpp
+++ AtCoder/ABC165/ABC165A.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+    int k, a, b;
+    cin >> k >> a >> b;
 
+    string answer = "NG";
+    for (int i = a; i <= b; i++) {
+        if (i % k == 0) answer = "OK";
+    }
+
+    cout << answer;
 }
