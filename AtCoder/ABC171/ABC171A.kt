63a02abc2b227a04262ecbe0612c1449a1576299
Lawillia <30737406+LawilliaJPN@users.noreply.github.com>
2020-07-11 14:26:15 +0900

    [NEW] ABC 171 - A - Kotlin

---
+++ AtCoder/ABC171/ABC171A.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val alpha = readLine()!![0]
+
+    val answer = if (alpha.isUpperCase()) "A" else "a";
+    println(answer)
+}
--- AtCoder/ABC171/README.md
+++ AtCoder/ABC171/README.md
@@ -5,5 +5,6 @@ https://atcoder.jp/contests/abc171
 ## 解答済み
 - A - αlphabet
 	- C++ (GCC 9.2.1)
+	- Kotlin (1.3.71)
 - B - Mix Juice
 	- C++ (GCC 9.2.1)
